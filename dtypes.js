1f03a4eb1789d1fffcb41eaa48014fbdc9169ce2
tinninj <94772768+tinninj@users.noreply.github.com>
2023-05-30 17:18:36 -0500

    Update dtypes.js
    added different data type names

--- dtypes.js
+++ dtypes.js
@@ -1,1 +1,19 @@
+/* This docmment contains examples of the data types are availible for JavaScript. */
 
+//Numbers
+
+//BigInt
+
+//Strings
+
+//Booleans
+
+//Undefined
+
+//Null
+
+//Symbols
+
+//Arrays
+
+//Maps
